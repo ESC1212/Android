19b4a2b7569a23ece19c717e9a0f61cd6cb3ddfa
ESC1212 <eduardo.sc12@aluno.ifsc.edu.br>
2024-10-07 10:01:09 -0300

    Salvar

--- Aula/listagem/src/main/java/com/example/listagem/MainActivity.java
+++ Aula/listagem/src/main/java/com/example/listagem/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.listagem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    String[] nomes = new String[] {"Cadu", "Douglas", "Jesus"};
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listview = findViewById(R.id.listview);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1,nomes);
+        listview.setAdapter(arrayAdapter);
     }
 }
