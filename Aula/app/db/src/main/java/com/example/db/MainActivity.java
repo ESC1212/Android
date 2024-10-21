9ec9c9a746483ae7e482f3dbfe673a7dcd1cfa91
Esc1212 <eduardo.sc12@aluno.ifsc.edu.br>
2024-10-21 09:59:33 -0300

    Save

--- Aula/app/db/src/main/java/com/example/db/MainActivity.java
+++ Aula/app/db/src/main/java/com/example/db/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.db;
 import androidx.appcompat.app.AppCompatActivity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase DB;
+    EditText txtNome,txtEmail,txtDataN;
+    Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SQLiteDatabase DB = openOrCreateDatabase("banco", getBaseContext().MODE_PRIVATE,null);
-        DB.execSQL("CREATE TABLE pessoas (id INT PRIMARY KEY AUTOINCREMENT,nome varchar, email varchar,datan DATE)");
+        DB.execSQL("CREATE TABLE pessoas (id INTEGER PRIMARY KEY AUTOINCREMENT,nome varchar, email varchar,datan DATE)");
+        txtNome = findViewById(R.id.txtNome);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtDataN = findViewById(R.id.txtDataN);
     }
 }
