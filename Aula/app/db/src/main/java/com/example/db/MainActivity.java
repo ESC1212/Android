4c90e4d799b2f87adc02dd6a989ee21ab1dacdf0
Esc1212 <eduardo.sc12@aluno.ifsc.edu.br>
2024-10-21 08:48:28 -0300

    Save

---
+++ Aula/app/db/src/main/java/com/example/db/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.db;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SQLiteDatabase DB = openOrCreateDatabase("banco", getBaseContext().MODE_PRIVATE,null);
+        DB.execSQL("CREATE TABLE pessoas (id INT PRIMARY KEY AUTOINCREMENT,nome varchar, email varchar)");
+    }
+}
